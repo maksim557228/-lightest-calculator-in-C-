140743a451c1a06b8de9d3796ddcd6d74e3c7298
user <user@WIN-7KSQPFQQOOT>
2023-08-22 14:20:04 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите первое число:");
+                int a = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите второе число:");
+                int b = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("-------------------------------------");
+                Console.WriteLine(("\nРезультат добавления = ") + (a + b) + "\nРезультат отнимания =  " + (a - b)
+                    + "\nРезультат умножения = " + (a * b) + "\nРезультат деления = " + (a / b));
+                Console.WriteLine("Хотите продолжить? (да/нет)");
+                string conti = Console.ReadLine();
+                if (conti.ToLower() != "да")
+                    break;
+                
+            }  
+        }
+    }
+}
